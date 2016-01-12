d33c78e83188bd6623485d3ebb8c0d647217be0b
Neville Ryant <nryant@tesla.ldc.upenn.edu>
2016-01-11 19:29:07 -0500

    Benchmark using d1b8333effdcb031e6e34a2835a2f1c877fdd79b.

--- feedforward_bench/run.sh
+++ feedforward_bench/run.sh
@@ -4,4 +4,7 @@
 #PYTHONPATH=../:$PYTHONPATH ./bench.py 0.6.0_timings.txt &> 0.6.0.log
 
 # Benchmark using b1cabed4e60015602dacd66ea39d419db50c3e1b
-PYTHONPATH=../:$PYTHONPATH ./bench.py b1cabed_timings.txt &> b1cabed.log
+#PYTHONPATH=../:$PYTHONPATH ./bench.py b1cabed_timings.txt &> b1cabed.log
+
+# Benchmark using d1b8333effdcb031e6e34a2835a2f1c877fdd79b
+PYTHONPATH=../:$PYTHONPATH ./bench.py d1b8333_timings.txt &> d1b8333.log
